59f3bf51ebfd753a36e568b381628359eb9e9530
Bhavani <bhavanivamshee@gmail.com>
2021-11-19 07:35:37 -0600

    builds landing page

---
+++ mental_health_client/src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <main>
+            <div className= "app-details">
+                <h1 className="app-title"> Mental Health Tracker </h1>
+                <p className="app-intro"> Manage your medicaitons, providers, behavior and more!</p>
+            </div>
+        </main>
+    )
+}
